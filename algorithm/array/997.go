f17bcb3806577398ea613ab905d4eac9a8dead9a
Gaius <gaius.qi@gmail.com>
2023-07-20 19:40:46 +0800

    feat: add 27, 704, 997 to array
    Signed-off-by: Gaius <gaius.qi@gmail.com>

---
+++ algorithm/array/997.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{-7, -3, 2, 3, 11}
+	fmt.Println(sortedSquares(nums))
+}
+
+func sortedSquares(nums []int) []int {
+	n := len(nums)
+	i, j, k := 0, n-1, n-1
+	ans := make([]int, n)
+	for i <= j {
+		lm, rm := nums[i]*nums[i], nums[j]*nums[j]
+		if lm > rm {
+			ans[k] = lm
+			i++
+		} else {
+			ans[k] = rm
+			j--
+		}
+		k--
+	}
+	return ans
+}
